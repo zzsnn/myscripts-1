17fb49d29695d2226246a039e49e30101bb45bae
小朋 <2810580330@qq.com>
2021-10-11 00:00:02 +0800

    Update xmyd.js

--- xmyd.js
+++ xmyd.js
@@ -83,7 +83,7 @@ function login(timeout = 0) {
         'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1'
       },
     }
-    $.post(url, async (err, resp, data) => {
+    $.get(url, async (err, resp, data) => {
       try {
         if (data.match(/success/) == 'success') {
           $.log(`\n刷步数成功：${step}步`)
